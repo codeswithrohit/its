c5abd754172497c4111a4e6475b5aa3dced7e2bf
codeswithrohit <hrohit320@gmail.com>
2024-11-17 12:40:47 +0530

    jhvh

--- src/component/TradingChart.js
+++ src/component/TradingChart.js
@@ -1,26 +1,9 @@
-import React from 'react';
-import TradeViewChart from 'react-crypto-chart';
+import React from 'react'
 
-export default function App() {
+const TradingChart = () => {
   return (
-    <div className="overflow-hidden">
-      <h3 className="font-[Lato]">BTC/USDT</h3>
-      <div
-        style={{
-          width: '100%', // Make it responsive
-          height: '400px', // Fixed height can still be used for the chart container
-          marginBottom: '30px',
-        }}
-      >
-        <TradeViewChart
-        containerStyle={{
-          minHeight: '300px', // Ensure minimum height for the chart
-          width: '100%', // Let the width adjust based on the parent container
-          marginBottom: '30px',
-        }}
-          pair="BTCUSDT"
-        />
-      </div>
-    </div>
-  );
+    <div>TradingChart</div>
+  )
 }
+
+export default TradingChart
